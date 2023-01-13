dcdcc7f88902d3f08badefc944494211f84af049
YURI JUNG <dbfl14433@gmail.com>
2023-01-13 17:32:22 +0900

    react 브랜치 연결테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from "./Test";
+import Loaging from "./Loaging";
 
 function App() {
     return (
         <div className="App">
             <Test />
+            <Loaging/>
         </div>
     );
 }
