195d0d6366acaaf97a23cb1c3c3444c42cec52ab
eblyncho <eblyn52@naver.com>
2023-01-16 15:30:25 +0900

    Main carousel 작업중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from "./Test";
-import Main from "./Main";
+import Main from "./Main/Main";
 
 function App() {
     return (
-        <div className="App">
+        <div>
             {/*<Test />*/}
             <Main />
         </div>
