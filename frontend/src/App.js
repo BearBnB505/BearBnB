41619c1b1b4cafa2bea11530ed33bce1470a086d
이용준 <dldydwns333@naver.com>
2023-01-16 09:28:51 +0900

    회원가입 초안

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from "./Test";
+import Join from "./Join";
+
 
 function App() {
     return (
         <div className="App">
-            <Test />
+            {/*<Test />*/}
+            <Join />
         </div>
     );
 }
