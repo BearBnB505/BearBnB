c09504651afcf7fac66be67a5ef0cb336045a9a6
severrepair <dldydwns333@naver.com>
2023-01-16 16:45:00 +0900

    2023-01-16 회원가입

--- frontend/src/Join.jsx
+++ frontend/src/Join.jsx
@@ -1,66 +1,97 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 
-
-function Join(props){
+function Join(props) {
 
 
     const [Email, setEmail] = useState("");
     const [Name, setName] = useState("");
-    const[Password, setPassword] = useState("");
+    const [Password, setPassword] = useState("");
     const [ConfirmPassword, setConfirmPassword] = useState("");
+    const [TellNumber, setTellNumber] = useState("");
+
 
     const onEmailHandler = (event) => {
         setEmail(event.currentTarget.value);
     }
-    const onNameHandler = (event) =>{
+    const onNameHandler = (event) => {
         setName(event.currentTarget.value);
     }
     const onPasswordHandler = (event) => {
         setPassword(event.currentTarget.value);
     }
-    const onConfirmPasswordHandler = (event) =>{
+    const onConfirmPasswordHandler = (event) => {
         setConfirmPassword(event.currentTarget.value);
     }
-    const onSubmitHandler = (event)=>{
+    const onTellHandler = (event) => {
+        setTellNumber(event.currentTarget.value);
+    }
+
+
+    const onSubmitHandler = (event) => {
         event.preventDefault();
 
-        if(Password !== ConfirmPassword){
+        if (Password !== ConfirmPassword) {
             return alert('비밀번호와 비밀번호 확인이 같지 않습니다.')
         }
 
         let body = {
             email: Email,
-            name:Name,
-            password:Password,
-            confirmPassword:ConfirmPassword,
+            name: Name,
+            password: Password,
+            confirmPassword: ConfirmPassword,
+            tellNumber: TellNumber
         }
 
     }
 
-    return(
-        <div style ={{
-            display:'flex',justifyContent: 'center', alignItems: 'center',
-            width:'100%',height: '100vh'
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
         }}>
-        <form style={{ display:'flex', flexDirection: 'column'}}
-            onSubmit={onSubmitHandler}
-              >
-            <label>Email</label>
-            <input type='email' value={Email} onChange={onEmailHandler}/>
-            <label>Name</label>
-            <input type='text' value={Name} onChange={onNameHandler}/>
-            <label>Password</label>
-            <input type='password' value={Password} onChange={onPasswordHandler}/>
-            <label>Confirm Password</label>
-            <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
-            <br />
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                  onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler}/>
+                <label>Name</label>
+                <input type='text' value={Name} onChange={onNameHandler}/>
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler}/>
+                <label>Confirm Password</label>
+                <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
+                <label>TellNumber</label>
+                <input type='tel' value={TellNumber} onChange={onTellHandler}/>
+                <br />
+                <label>
+                    성별:
+                    <select>
+                        <option value="남자">남자</option>
+                        <option value="여자">여자</option>
+                    </select>
+                </label>
+                <br />
+                <label>
+                    국가:
+                    <select>
+                        <option>아시아</option>
+                        <option>북아메리카</option>
+                        <option>남아메리카</option>
+                        <option>유럽</option>
+                        <option>오세아니아</option>
+                        <option>아프리카</option>
+                    </select>
+                </label>
+                <h1>생년월일 지정하기</h1>
+                <input type={"date"}/>
+            <br/>
             <button formAction=''>
                 회원가입
             </button>
         </form>
-        </div>
-    )
+</div>
+)
 
 }
 
