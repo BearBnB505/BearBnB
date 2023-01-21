85b1be3e4e95f32225c9e14acb22c989043c2ab2
Eblyn <eblyn52@naver.com>
2023-01-21 18:20:57 +0900

    header 메뉴 선택 modal 적용, 게스트 수 선택 컴포넌트 편집

--- frontend/src/GuestCount.jsx
+++ frontend/src/GuestCount.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
-function Guest(){
+function GuestCount() {
 
     const [adultCount , setAdultCount] = useState(0);
     const [childCount , setChildCount] = useState(0);
@@ -9,8 +9,7 @@ function Guest(){
 
     const styles = {
         con : {
-            marginLeft : "250px",
-            marginTop: "200px"
+
         },
         font : {
             position : "flex",
@@ -22,7 +21,6 @@ function Guest(){
         img : {
             height : "25px",
             width : "25px",
-
         },
         input : {
             border : "none",
@@ -31,8 +29,7 @@ function Guest(){
             fontSize: "20px"
         },
         hr:{
-            // marginTop: "-10px",
-            width :"420px",
+            width :"350px",
             textAlign: "center",
             margin:"auto",
 
@@ -78,27 +75,26 @@ function Guest(){
     }
 
     return(
-        <div className ={"container"}>
+        <div className ={"container px-4"}>
             <div className={"row"} >
-                <div className={"col-4"} style={styles.con}>
+                <div className={"col"} style={styles.con}>
                     {/*<p className={"fw-bold"} style={styles.font}>숙소 기본 정보를 알려주세요</p>*/}
                     {/*<p className={"fs-5 fw-light"}>숙소에 머물 수 있는 게스트 수, 침실 수, 침대 수, 욕실 수를 지정해주세요 </p>*/}
                     <div className = "col-sm-12">
                         {/*성인*/}
-                        <div className="row justify-content-between mt-5 p-1 mb-3">
+                        <div className="row justify-content-between mt-4 p-1 mb-3">
                                 <div className="col-6 fs-5" >
                                     성인<br/> <p style={{color:"gray", fontSize:"17px"}}>만 2세 미만</p>
                                 </div>
-                            <div className = "col-1">
+                            <div className = "col-1 p-0">
                                     <img src="/concept/minusIcon.png" style={styles.img} onClick={AdultMinus}/>
                             </div>
-                            <div className={"col-1"}>
+                            <div className={"col-1 p-0"}>
                                 <input type={"text"} value={adultCount} style={styles.input} />
                             </div>
-                            <div className={"col-1"}>
+                            <div className={"col-1 p-0"}>
                                 <img src="/concept/plusIcon.png" style={styles.img} onClick={AdultPlus}/>
                             </div>
-
                         </div>
 
                         <hr style={styles.hr}/>
@@ -108,13 +104,13 @@ function Guest(){
                             <div className="col-6 fs-5" >
                                 유아 <br/> <p style={{color:"gray", fontSize:"17px"}}>만 2세 미만</p>
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <img src="/concept/minusIcon.png" style={styles.img} onClick={ChildMinus}/>
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <input type={"text"} value={childCount} style={styles.input} />
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <img src="/concept/plusIcon.png" style={styles.img} onClick={ChildPlus}/>
                             </div>
                         </div>
@@ -125,21 +121,21 @@ function Guest(){
                             <div className="col-6 fs-5" >
                                 반려동물<p style={{color:"gray", fontSize:"15px", textDecorationLine: "underline"}}>반려동물을 동반하시나요?</p>
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <img src="/concept/minusIcon.png" style={styles.img} onClick={PetMinus}/>
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <input type={"text"} value={petCount} style={styles.input} />
                             </div>
-                            <div className="col-1">
+                            <div className="col-1 p-0">
                                 <img src="/concept/plusIcon.png" style={styles.img} onClick={PetPlus}/>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <Link to ={"/lodgingLocationDetail"}><button className={"btn btn-light position-absolute start-0 bottom-0 ms-5 mb-3"} >이전</button></Link>
-                        <Link to = {"/lodgingBedSelect"}><button className={"btn btn-primary position-absolute end-0 bottom-0 me-5 mb-3"}>다음</button></Link>
-                    </div>
+                    {/*<div>*/}
+                    {/*    <Link to ={"/lodgingLocationDetail"}><button className={"btn btn-light position-absolute start-0 bottom-0 ms-5 mb-3"} >이전</button></Link>*/}
+                    {/*    <Link to = {"/lodgingBedSelect"}><button className={"btn btn-primary position-absolute end-0 bottom-0 me-5 mb-3"}>다음</button></Link>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
@@ -147,4 +143,4 @@ function Guest(){
     );
 
 }
-export default Guest;
+export default GuestCount;
---
+++ frontend/src/Header/HeaderModal.jsx
@@ -0,0 +1,125 @@
+import React, {useState} from "react";
+import {Dropdown, Modal, ModalBody} from "react-bootstrap";
+
+import './Header.css';
+import Header from "./Header";
+import {Link} from "react-router-dom";
+import DropdownToggle from "react-bootstrap/DropdownToggle";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
+import DropdownItem from "react-bootstrap/DropdownItem";
+
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import GuestCount from "../GuestCount";
+
+function HeaderModal(props) {
+    const styles = {
+        pillStart: {
+            borderTopLeftRadius: 25,
+            borderBottomLeftRadius: 25
+        },
+        nav1PillEnd: {
+            borderTopRightRadius: 25,
+            borderBottomRightRadius: 25,
+            width: 160
+        },
+        nav2PillEnd: {
+            borderTopRightRadius: 25,
+            borderBottomRightRadius: 25,
+            width: 200
+        },
+    }
+
+    const [show, setShow] = useState(false);
+
+    return (
+        <>
+            <div className={"nav-item"} id={"nav-first"}>
+                <div className={"btn-group"} role={"group"}>
+
+                    <button type={"button"} id={"btn-where"} className={"btn btn-outline-secondary px-3 py-1"} style={styles.pillStart} onClick={() => setShow(true)}>어디든지</button>
+                    <button type={"button"} className={"btn btn-outline-secondary px-3 py-1"}>언제든 일주일</button>
+                    <button type={"button"} className={"btn btn-outline-secondary px-3 py-1"} style={styles.nav1PillEnd}>
+                        <div className={"row"}>
+                            <div className={"col-8 m-0 px-0 py-2"}>
+                                게스트 추가
+                            </div>
+                            <div className={"col-1 m-0"}>
+                                <button type={"button"} className={"btn btn-primary "} style={{borderRadius: 30, paddingLeft: 10, paddingRight: 10, height: 34, marginTop:3}}>
+                                    <FontAwesomeIcon icon={faMagnifyingGlass} style={{width: 12, paddingBottom:2}}/>
+                                </button>
+                            </div>
+                        </div>
+                    </button>
+                </div>
+            </div>
+
+            <Modal
+                show={show}
+                onHide={() => setShow(false)}
+                dialogClassName={"header-modal"}
+                animation={false}
+            >
+                <ModalBody>
+                    <nav className={"navbar navbar-expand-sm navbar-white bg-white mt-1 fixed"} id={"header_nav"}>
+                        <div className={"container justify-content-between"} style={{width: 1900}}>
+                            <Link to={"/"}><img src="/img/BearBnB_logo.png" alt="BearBnB" style={{width:130}}/></Link>
+
+                            <div className={"nav-item"}>
+                                <Dropdown>
+                                    <DropdownToggle variant={"none"} bsPrefix style={{border:"none"}}>
+                                        <a href="#" className={"nav-link"}><img src="/img/user.png" alt="user" style={{width: 35}}/></a>
+                                    </DropdownToggle>
+
+                                    <DropdownMenu align={"end"}>
+                                        <DropdownItem href={"#"}>로그인</DropdownItem>
+                                        <DropdownItem href={"/join"}>회원가입</DropdownItem>
+                                        <DropdownItem href={"/mypage"}>마이페이지</DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
+                            </div>
+                        </div>
+                    </nav>
+                    <nav className={"mb-2"} id={"nav-second"}>
+                        <div className={"container d-flex justify-content-center"}>
+                            <div className={"btn-group sidenav"} role={"group"}  id={"menu"}>
+                                <button type={"button"} className={"btn btn-outline-secondary px-3 py-2"} style={styles.pillStart}>여행지</button>
+                                <button type={"button"} className={"btn btn-outline-secondary px-3 py-2"}>체크인</button>
+                                <button type={"button"} className={"btn btn-outline-secondary px-3 py-2"}>체크아웃</button>
+                                <button type={"button"} className={"btn btn-outline-secondary px-3 py-2"} style={styles.nav2PillEnd}>
+                                    <div className={"row"}>
+                                        <div className={"col px-0 py-2"}>
+                                            <Dropdown drop={"down-centered"}>
+                                                <DropdownToggle variant={"none"} bsPrefix style={{border:"none"}}>
+                                                    <a href="#" className={"nav-link"}>여행자</a>
+                                                </DropdownToggle>
+
+                                                <DropdownMenu>
+                                                    <GuestCount />
+                                                </DropdownMenu>
+                                            </Dropdown>
+                                        </div>
+                                        <div className={"col-8"} style={{paddingTop:1}}>
+                                            <button type={"button"} className={"btn btn-primary"} style={{borderRadius:25}}>
+                                                <div className={"row px-1"}>
+                                                    <div className={"col-1"}>
+                                                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{width: 12, paddingTop: 4}}/>
+                                                    </div>
+                                                    <div className={"col-6 p-0 ms-1 me-2"}>
+                                                        <span>검색</span>
+                                                    </div>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </nav>
+                </ModalBody>
+            </Modal>
+        </>
+    );
+}
+
+export default HeaderModal;
