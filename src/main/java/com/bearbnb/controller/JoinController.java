c34ea25532ea01eb1254b35566d1d90fe6f25eb0
YURI JUNG <dbfl14433@gmail.com>
2023-02-07 20:25:55 +0900

    join test

---
+++ src/main/java/com/bearbnb/controller/JoinController.java
@@ -0,0 +1,18 @@
+//package com.bearbnb.controller;
+//
+//import com.bearbnb.dto.LodgingDto;
+//import com.bearbnb.dto.MembersDto;
+//import org.springframework.web.bind.annotation.*;
+//
+//@CrossOrigin(origins = {"http://localhost:3000"})
+//@RestController
+//public class JoinController {
+//
+//    @RequestMapping(value = "/insertJoin", method = RequestMethod.POST)
+//    public String insertLodgingTable(@RequestBody MembersDto members) throws Exception{
+//        System.out.println(members);
+//
+//        return null;
+//    }
+////        regLodgingService.insertLodgingTable(lodging);
+//}
---
+++ src/main/java/com/bearbnb/service/JoinService.java
@@ -0,0 +1,4 @@
+//package com.bearbnb.service;
+//
+//public class JoinService {
+//}
