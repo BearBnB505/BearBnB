5a7b1ef7a73d1f1481c652e88d098f7b05218b4f
YURI JUNG <dbfl14433@gmail.com>
2023-02-08 23:15:22 +0900

    비밀번호 passwordEncoder 적용 성공

--- src/main/java/com/bearbnb/service/JoinServiceImpl.java
+++ src/main/java/com/bearbnb/service/JoinServiceImpl.java
@@ -4,6 +4,7 @@ import com.bearbnb.dto.LodgingDto;
 import com.bearbnb.dto.MembersDto;
 import com.bearbnb.mapper.JoinMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
 public class JoinServiceImpl implements JoinService{
@@ -11,8 +12,14 @@ public class JoinServiceImpl implements JoinService{
     @Autowired
     JoinMapper joinMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public MembersDto insertJoin(MembersDto members) throws Exception{
+//        비밀번호 암호화 적용
+        String encodedPassword = passwordEncoder.encode(members.getPwd());
+        members.setPwd(encodedPassword);
         joinMapper.insertJoin(members);
 
         return members;
