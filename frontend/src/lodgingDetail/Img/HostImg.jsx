8fd193eee660b4d17d8506ae4e12b3239e75df96
Yuri-Jung <dbfl14433@gmail.com>
2023-02-13 12:15:30 +0900

    이미지 작업중

--- frontend/src/lodgingDetail/Img/HostImg.jsx
+++ frontend/src/lodgingDetail/Img/HostImg.jsx
@@ -6,11 +6,14 @@ import HostImgModalDetail from "./HostImgModalDetail";
 function HostImg({photo}) {
     const [modal, setModal] = useState(false);
     console.log('여기서 잘 전달되었는지 테스트')
+    console.log(photo)
     //이렇게하면 사진 이미지가 나온다.
-    // console.log({photo}.photo.data[0].photo)
+    console.log({photo}.photo.data[0].photo)
 
     const imageList = {photo}.photo.data;
+
     console.log(imageList);
+
 
     // const photoList = {photo}
     // console.log('photoList')
@@ -23,7 +26,17 @@ function HostImg({photo}) {
 
     return (
         <>
-            <div className={'container'}>
+            <div className={'containerGrid'}>
+                {/*화면 출력 확인*/}
+                <img src={{photo}.photo.data[0].photo} className={'img1'}/>
+                <img src={{photo}.photo.data[1].photo} className={'img2'}/>
+                <img src={{photo}.photo.data[2].photo} className={'img3'}/>
+                <img src={{photo}.photo.data[3].photo} className={'img4'}/>
+                <img src={{photo}.photo.data[4].photo} className={'img5'}/>
+                <Button className={'HouseImgBtn'} variant="dark"
+                        onClick={() => setModal(true)}>
+                    사진 모두보기</Button>
+
                 {/*<div className={'row'}>*/}
                 {/*<div className={'col'}>*/}
                 {/*    <img src={{photo}.photo.data[0].photo} className={'img1'}/>*/}
@@ -39,7 +52,9 @@ function HostImg({photo}) {
 
                 {/*</div>*/}
             </div>
-                {/*<ul className={"ps-1 d-flex flex-wrap"}>*/}
+            {/*<img className={'img1'} src={imageList.photo.data[0].photo}/>*/}
+
+                {/*일단 나오는 거 확인*/}
                 {/*{*/}
                 {/*    imageList.map((item) => {*/}
                 {/*        return (*/}
@@ -53,17 +68,22 @@ function HostImg({photo}) {
                 {/*        )*/}
                 {/*    })*/}
                 {/*}*/}
-                {/*</ul>*/}
-            {/*</div>*/}
-            {/*<div className={'containerGrid'}>*/}
-            {/*<HostImgItem photo={photo}/>*/}
-            <Button className={'HouseImgBtn'} variant="dark"
-                    onClick={() => setModal(true)}>
-                사진 모두보기</Button>
+
+
+                {/*<div className={'containerGrid'}>*/}
+                {/*    <img className={'img1'} src={imageList.photo.data[0].photo}/>*/}
+                {/*    <img className={'img2'} src={imageList.photo.data[1].photo}/>*/}
+                {/*    <img className={'img3'} src={imageList.photo.data[2].photo}/>*/}
+                {/*    <img className={'img4'} src={imageList.photo.data[3].photo}/>*/}
+                {/*    <img className={'img5'} src={imageList.photo.data[4].photo}/>*/}
+                {/*</div>*/}
+
+
+
             {/*</div>*/}
 
-            <HostImgModalDetail photo={photo}
-                                show={modal} onHide={()=>setModal(false)} />
+            {/*<HostImgModalDetail photo={photo}*/}
+            {/*                    show={modal} onHide={()=>setModal(false)} />*/}
 
             <hr/>
             {/*</div>*/}
