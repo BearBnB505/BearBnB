0b3ae0c340a202a5392e7ec41687e8de3998292e
eblyncho <eblyn52@naver.com>
2023-02-14 09:30:04 +0900

    사라진 코드 데려오기

--- frontend/src/GoogleMap/Search.jsx
+++ frontend/src/GoogleMap/Search.jsx
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import {LoadScript, MarkerF} from '@react-google-maps/api';
 import {GoogleMap, StandaloneSearchBox} from "@react-google-maps/api";
 import * as gvar from "./global_variables";
-import {basicInfo} from "../lodging_reg/Reducers/LodgingbedReducer";
+import {Link} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {maps} from "../lodging_reg/Reducers/LodgingMapReducer";
-import {Link} from "react-router-dom";
+
+const libraries = ['places'];
+
 
 function Search(props) {
 
@@ -160,3 +162,46 @@ function Search(props) {
 }
 
 export default Search;
+
+export function SearchHeader(props) {
+
+    const styles = {
+        navInputSpot: {
+            // cursor:"pointer",
+            width:220,
+            fontSize:15
+        },
+    }
+
+    const [searchBox, setSearchBox] = useState(null);
+
+    function handleLoad(ref) {
+        setSearchBox(ref);
+    }
+
+    function handlePlacesChanged() {
+        console.log(searchBox.getPlaces());
+        // console.log(searchBox.getPlaces()[0].name);
+        const spotName = searchBox.getPlaces()[0].name;
+        console.log("search : " + spotName);
+        // console.log(searchBox.getPlaces()[0].geometry.location.lat());
+        // console.log(searchBox.getPlaces()[0].geometry.location.lng());
+        // const lat = searchBox.getPlaces()[0].geometry.location.lat();
+        // const lng = searchBox.getPlaces()[0].geometry.location.lng();
+        props.spot(spotName);
+    }
+
+    return (
+        <div className={"px-0"}>
+            <LoadScript id="script-loader" googleMapsApiKey={gvar.BEE_API_KEY} libraries={libraries} zoom={10}>
+                <StandaloneSearchBox
+                    onLoad={handleLoad}
+                    onPlacesChanged={handlePlacesChanged}
+                    style={{zIndex:9999}}
+                >
+                    <input type="text" id={"chooseSpot"} className={"border-0 p-0"} placeholder={"여행지 검색"} style={styles.navInputSpot}/>
+                </StandaloneSearchBox>
+            </LoadScript>
+        </div>
+    )
+}
