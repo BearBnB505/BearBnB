5a9af1a70a6b886a5cd600f11bb98e3bb7f6d9e0
YURI JUNG <dbfl14433@gmail.com>
2023-03-03 16:15:14 +0900

    프로필 사진 등록 디버깅 성공

--- frontend/src/Mypage/MyPageProfile.jsx
+++ frontend/src/Mypage/MyPageProfile.jsx
@@ -6,10 +6,9 @@ import Swal from "sweetalert2";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {Auth} from "../Auth/Auth";
-import {CheckToken} from "../Auth/CheckToken";
 
 function MyPageProfile() {
-    const [img, setImg] = useState("https://cdn-icons-png.flaticon.com/512/3177/3177440.png")
+    // const [img, setImg] = useState("https://cdn-icons-png.flaticon.com/512/3177/3177440.png")
     const imageInput = useRef();
     //서버 올리기 전
     const [selectImage, setSelectImage] = useState('');
@@ -20,9 +19,9 @@ function MyPageProfile() {
 
     //db에 저장되어있던 기존 이미지
     const [dbImage,setDbImage] = useState('');
-
     const auth = Auth();
     const userId = auth.userId;
+
 
     useEffect(()=>{
         axios.get('http://localhost:8080/selectProfile',{
@@ -38,8 +37,8 @@ function MyPageProfile() {
             })
     },[userId])
 
-    // console.log('가져온 이미지 데이터 확인')
-    // console.log(dbImage);
+    console.log('가져온 이미지 데이터 확인')
+    console.log(dbImage);
 
 
 
@@ -51,8 +50,8 @@ function MyPageProfile() {
         setSelectImage(URL.createObjectURL(event.target.files[0]));
         setProfileImg(event.target.files[0]);
     }
-    // console.log("serverHostIdImage");
-    // console.log(profileImg);
+    console.log("serverHostIdImage");
+    console.log(profileImg);
 
     const navigate = useNavigate();
     const onSubmit = (event) => {
@@ -84,43 +83,38 @@ function MyPageProfile() {
         const dayPlusRandom = dayday + random;
         // 이미지 이름=> 날짜6자리 + 랜덤 숫자 11자리
         const imgName = dayPlusRandom.split(',').join("");
-        // console.log(imgName);
+        console.log(imgName);
 
 
         // 업로드 처리
-        // console.log("업로드 처리");
+        console.log("업로드 처리");
         const storageRef = storage.ref("BearBnB/profile/"); //어떤 폴더 아래에 넣을지 설정
         const imagesRef = storageRef.child(imgName + profileImg.name); //파일명
 
-        // console.log("파일을 업로드하는 행위");
+        console.log("파일을 업로드하는 행위");
         const upLoadTask = imagesRef.put(profileImg);
-
-
-        const auth = Auth();
-        console.log("auth.userId: " + auth.userId);
-        const userId = auth.userId;
 
         upLoadTask.on(
             "state_changed",
             (snapshot) => {
-                // console.log("snapshot", snapshot);
+                console.log("snapshot", snapshot);
             },
             (error) => {
-                // console.log("err", error);
+                console.log("err", error);
             },
             () => {
                 upLoadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-                        // console.log("File available at", downloadURL);
+                        console.log("File available at", downloadURL);
                         setFirebaseUrl(downloadURL);
-                        // console.log('업로드 성공');
-                        // console.log('url 데이터 이동성공');
-                        // console.log(firebaseUrl);
+                        console.log('업로드 성공');
+                        console.log('url 데이터 이동성공');
+                        console.log(firebaseUrl);
 
                         //    axios 통신
                         axios.post('http://localhost:8080/insertProfile', null,
                             {
                                 params: {
-                                    userId: "dbfl1443@naver.com",
+                                    userId: userId,
                                     profileImg: downloadURL,
                                 }
                             })
@@ -135,14 +129,14 @@ function MyPageProfile() {
                                     timer: 1500
                                 }).then(() => {
                                     // setShow(false);
-                                    // console.log('프로필 통신 성공')
+                                    console.log('프로필 통신 성공')
                                 })
                                 navigate(-1);
 
                             })
                             .catch((error) => {
                                 console.log("사진 업로드 후 통신 에러")
-                                // console.log(error)
+                                console.log(error)
                             })
                     }
                 )
@@ -150,39 +144,39 @@ function MyPageProfile() {
     }
 
 
-        return (
-            <div className={'container'}>
-                <div className={'row'}>
-                    <div className={'col'}>
-                        <div className={'col-6'}>
-                            <input type={'file'} style={{display: "none"}} ref={imageInput} accept="image/*"
-                                   onChange={handleImage}/>
-                        </div>
-                    </div>
-                </div>
-                {/*src={idImageUrl.length<=0 ? '/concept/plusIcon.png' : idImageUrl}*/}
-                <div className={"row"}>
-                    <div className={'col-10'}>
-                        <div style={{textAlign: 'center'}}>
-                            <img
-                                src={profileImg.length < 1 ? dbImage.data : selectImage}
-                                style={{borderRadius: "70%", width: "300px", marginTop: "90px"}}/>
-                        </div>
-                        <div style={{textAlign: 'center', marginTop: "30px"}}>
-                            <button className={'btn btn-outline-primary'} onClick={onClickImageUpload}>프로필 선택</button>
-                        </div>
-                        <div style={{textAlign: 'center', marginTop: "30px"}}>
-                            <button className={'btn btn-outline-primary'} onClick={onSubmit}>프로필 저장하기</button>
-                        </div>
-                    </div>
-
-                    <div className={'col-2'}>
-                        {/*<button>이미지 선택</button>*/}
-                        {/*<button>저장</button>*/}
+    return (
+        <div className={'container'}>
+            <div className={'row'}>
+                <div className={'col'}>
+                    <div className={'col-6'}>
+                        <input type={'file'} style={{display: "none"}} ref={imageInput} accept="image/*"
+                               onChange={handleImage}/>
                     </div>
                 </div>
             </div>
-        )
+            {/*src={idImageUrl.length<=0 ? '/concept/plusIcon.png' : idImageUrl}*/}
+            <div className={"row"}>
+                <div className={'col-10'}>
+                    <div style={{textAlign: 'center'}}>
+                        <img
+                            src={profileImg.length < 1 ? dbImage.data : selectImage}
+                            style={{borderRadius: "70%", width: "300px", marginTop: "90px"}}/>
+                    </div>
+                    <div style={{textAlign: 'center', marginTop: "30px"}}>
+                        <button className={'btn btn-outline-primary'} onClick={onClickImageUpload}>프로필 선택</button>
+                    </div>
+                    <div style={{textAlign: 'center', marginTop: "30px"}}>
+                        <button className={'btn btn-outline-primary'} onClick={onSubmit}>프로필 저장하기</button>
+                    </div>
+                </div>
+
+                <div className={'col-2'}>
+                    {/*<button>이미지 선택</button>*/}
+                    {/*<button>저장</button>*/}
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
